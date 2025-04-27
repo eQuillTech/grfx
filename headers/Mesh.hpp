aec21783f6eb528eb1960b79415d928524329b12
Phil Ahrenkiel <61021639+spahrenk@users.noreply.github.com>
2025-04-26 18:04:20 -0600

    Remove file...

--- headers/Mesh.hpp
+++ headers/Mesh.hpp
@@ -132,29 +132,6 @@ public:
 	double size(std::size_t iDim){return _range.max()(iDim)-_range.min()(iDim);}
 };
 
-//Wavefront vertex assignment
-struct WFTIndex
-{
-	union
-	{
-		struct
-		{
-			long _iPos,_iNorm,_iTex;
-		};
-		long _p[3]; //array representation
-	};
-	WFTIndex(){}
-	WFTIndex(const long iPos,const long iNorm,const long iTex):
-		_iPos(iPos),_iNorm(iNorm),_iTex(iTex){}
-	bool operator==(const WFTIndex &rhs) const
-	{
-		return ((_p[0]==rhs._p[0])&&(_p[1]==rhs._p[1])&&(_p[2]==rhs._p[2]));
-	}
-};
-
-typedef std::vector<WFTIndex> WFTIndexVector;
-std::size_t findIndexOrPushVertex(WFTIndexVector &wftIndexVector,const WFTIndex &wftIndex);
-
 //
 enum FragmentMode
 {
